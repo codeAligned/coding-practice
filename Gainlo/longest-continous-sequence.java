93fc9a8bc9ac3b2a8595ca3b0faf6d5b482140f5
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2016-07-20 19:37:14 -0400

    Sample questions without answers.

---
+++ Gainlo/longest-continous-sequence.java
@@ -0,0 +1,12 @@
+/*
+
+Find longest contiguous sequence within array, that sums to 0. Array includes negative numbers.
+
+E.g.
+
+[-1 0 1] -> 3
+[2 3 1 -3 2 -2 3] -> 4
+
+*/
+
+// TODO: Longest continous sequence
---
+++ geeksforgeeks.org/overlapping-intervals.java
@@ -0,0 +1,47 @@
+/*
+
+http://www.practice.geeksforgeeks.org/problem-page.php?pid=126
+
+Given a collection of Intervals,merge all the overlapping Intervals.
+For example:
+
+Given [1,3],[2,6],[8,10],[15,18],
+
+return [1,6],[8,10],[15,18].
+
+Make sure the returned intervals are sorted.
+
+
+
+Input:
+
+The first line contains an integer T, depicting total number of test cases.
+Then following T lines contains an integer N depicting the number of Intervals
+and next line followed by the intervals starting and ending positions 'x' and 'y' respectively.
+
+
+Output:
+
+Print the intervals after overlapping in sorted manner.
+
+Note: There should be a space at the end of output of every test case.
+
+Constraints:
+
+1 ≤ T ≤ 30
+1 ≤ N ≤ 100
+0 ≤ x ≤ y ≤ 100
+
+
+Example:
+
+Input
+1
+4
+1 3 2 4 6 8 9 10
+Output
+1 4 6 8 9 10
+
+*/
+
+// TODO: Overlapping intervals
