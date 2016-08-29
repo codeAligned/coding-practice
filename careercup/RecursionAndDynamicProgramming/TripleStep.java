5c8d7a43774b3a01445c676df2c443f61fab1867
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2016-08-29 14:18:57 -0400

    Triple step.

--- careercup/RecursionAndDynamicProgramming/TripleStep.java
+++ careercup/RecursionAndDynamicProgramming/TripleStep.java
@@ -10,23 +10,31 @@ package careercup.recursionanddynamicprogramming;
 import helpers.java.Inputs;
 
 public class TripleStep {
+    private static int[] ways;
+
     private static int countDifferentWays(int n) {
-        int[] ways = new int[n];
+        if(ways[n] != 0) {
+            return ways[n];
+        }
+
+        int oneway = countDifferentWays(n - 1);
+        int twoway = countDifferentWays(n - 2);
+        int threeway = countDifferentWays(n - 3);
+
+        ways[n] = oneway + twoway + threeway;
+        return ways[n];
+    }
+
+    public static void main(String[] args) {
+        String[] inputs = Inputs.readAllInputs();
+        int n = Integer.parseInt(inputs[0]);
+
+        ways = new int[n + 1];
+        ways[0] = 0;
         ways[1] = 1;
         ways[2] = 2;
         ways[3] = 4;
 
-        int total = 0;
-        while(n > 0) {
-
-        }
-
-        return total;
-    }
-
-    private static void main(String[] args) {
-        String[] inputs = Inputs.readAllInputs();
-        int n = Integer.parseInt(inputs[0]);
         System.out.println(countDifferentWays(n));
     }
 }
