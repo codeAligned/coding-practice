ab03ba62519bd762ab94824e99f3d305676540de
Rahul Chaudhary <rahul300chaudhary400@gmail.com>
2016-09-19 06:23:55 -0400

    Dustin questions

---
+++ Friends/Dustin/paranthesis-combination.py
@@ -0,0 +1,11 @@
+"""
+Given n (even), print all possible valid paranthesis expressions.
+
+E.g. n = 6 can generate 4 possible combinations.
+()()()
+(())()
+()(())
+((()))
+"""
+
+# TODO Dustin - paranthesis combinations
---
+++ Friends/Dustin/word-difference.py
@@ -0,0 +1,9 @@
+"""
+Given two words, what number of steps are required to convert one to another.
+You can only replace, add, or delete a letter.
+
+E.g. gamma and agamma requires only one deletion.
+cat and kol requires 3 replace.
+"""
+
+# TODO Dustin - word difference
